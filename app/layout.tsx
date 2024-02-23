e0fbdb73881835078b1f6077d4059e302e1f5e94
Harjot Singh <HarjjotSinghh@gmail.com>
2024-02-23 23:58:18 +0530

    Initial commit using create-next-app

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={cn(GeistSans.className, "dark bg-background text-foreground")}>
+      <body className="">
+        <main className="min-h-screen flex flex-col items-center">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
