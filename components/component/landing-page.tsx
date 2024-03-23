1f3f688ea0359161cc1009b8ebeac39ef0e30efc
Harjot Singh <HarjjotSinghh@gmail.com>
2024-03-23 23:16:15 +0530

    redirect to /signin instead of /courses

--- components/component/landing-page.tsx
+++ components/component/landing-page.tsx
@@ -98,7 +98,7 @@ export default function LandingPage() {
                             </p>
                             <div className="flex flex-row items-start justify-start lg:gap-6 gap-2">
                                 <Link
-                                    href={'/courses'}
+                                    href={'/signin'}
                                     className="w-fit lg:text-lg text-base"
                                 >
                                     <Button
@@ -561,25 +561,5 @@ export default function LandingPage() {
         </div>
       </section> */}
         </div>
-    );
-}
-
-function BookOpenIcon({ ...props }) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-            <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
-        </svg>
     );
 }
